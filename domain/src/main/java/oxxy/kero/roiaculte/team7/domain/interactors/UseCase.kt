199ec2167feec40285796c088592d5fe6ab9d584
akram09 <a.boutouchent@esi-sba.dz>
2019-01-03 02:17:04 +0100

    configured the usecase class

--- domain/src/main/java/oxxy/kero/roiaculte/team7/domain/interactors/UseCase.kt
+++ domain/src/main/java/oxxy/kero/roiaculte/team7/domain/interactors/UseCase.kt
@@ -1,18 +1,3 @@
-/**
- * Copyright (C) 2018 Fernando Cejas Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package oxxy.kero.roiaculte.team7.domain.interactors
 
 import io.reactivex.Observable
@@ -25,11 +10,15 @@ import oxxy.kero.roiaculte.team7.domain.functional.Either
 
 interface EitherInteractor<in P, out R> {
     val dispatcher: CoroutineDispatcher
+    val ResultDispatcher :CoroutineDispatcher
+
     suspend operator fun invoke(executeParams: P):Either<Failure, R>
 }
 
 interface Interactor<in P>{
     val  dispatcher: CoroutineDispatcher
+    val ResultDispatcher :CoroutineDispatcher
+
     suspend operator fun invoke(executeParams: P)
 }
 
@@ -48,15 +37,16 @@ abstract class ObservableInteractor<Type , in Params>(private val schedulers:App
 }
 
 fun <P, R> CoroutineScope.launchInteractor(interactor: EitherInteractor<P,R >, param: P , OnResult:(Either<Failure, R>)->Unit): Job {
-   val  job = async { interactor(param) }
-    return launch(interactor.dispatcher) { OnResult(job.await()) }
+    val  job = async(interactor.dispatcher) { interactor(param) }
+    return launch(interactor.ResultDispatcher) { OnResult(job.await()) }
 }
 class None
 
 
 fun <P> CoroutineScope.launchInteractor(interactor:Interactor<P>, param: P, onResult:()->Unit):Job{
-    return launch (interactor.dispatcher){ interactor(param)
-                        onResult()}
+    val job = async(context = interactor.dispatcher){interactor(param)}
+    return launch (interactor.ResultDispatcher){ job.await()
+        onResult()}
 
 }
 fun  <R>CoroutineScope.launchInteractor(interactor: EitherInteractor<Unit, R>, OnResult: (Either<Failure, R>) -> Unit) =
